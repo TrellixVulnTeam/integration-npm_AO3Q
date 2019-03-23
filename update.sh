bc8e8b8f3d49249de2204d62df6d586722d6d758
Timo Tijhof <krinklemail@gmail.com>
2019-03-23 03:52:31 +0000

    wmf: Rewrite update.sh based on npmjs.org tarball
    There are two problems at the moment:
    
    1. Source files and make artefacts from previous versions are
       left behind (due to git-merge).
    
    2. Files that are meant to be excluded from the distribution
       (npmignore) are still kept, which might cause subtle
       differences. In particular, the Makefile installs an extra
       package (node_modules/marked) which is meant to be excluded.
    
    I was half-way trying to make point 1 and 2 work by using a
    combination of 'git reset $1', which looked as follows for the
    first part of the update script:
    
      git reset "$1" .
      git add .gitreview README-Wikimedia update.sh
      git clean -dffx
      git add .
      git commit -m "Checkout upstream $1"
    
    However that still didn't honour npmignore. It also still caused
    one other minor side-effect, which is that due to running Make
    ourselves, the dates in man files differ from the actual release.
    
    This commit instead fetches and unpacks the tarball of the official
    release. This is what npm uses when installing other packages,
    and also when it does self-updates. It's also what upstreams
    scary install.sh does.
    
    Bug: T215562
    Change-Id: I588a5f68d0508fca7223490e4d6a100d3c05febd

--- update.sh
+++ update.sh
@@ -3,13 +3,29 @@
 set -euxo pipefail
 
 echo "Going to update to $1"
-git fetch upstream
-# This git merge is OK to fail
-git merge $1 --allow-unrelated-histories --squash --strategy-option=theirs ||:
-git add .
-git commit -m "Merge $1"
-make clean
-make
-git add .
-git commit -m "Build $1"
+
+# Stash our files in memory
+our_gitreview=$(cat .gitreview)
+our_readme=$(cat README-Wikimedia)
+our_script=$(cat update.sh)
+
+# Wipe the whole working tree and stage its removal
+git rm -rf . && git clean -dffx
+
+# Download and extract the next version
+url="https://registry.npmjs.org/npm/-/npm-$1.tgz"
+curl -SsL "$url" | tar -xzf -
+mv package/.[!.]* package/* . && rmdir package/
+# Verify that it works
 ./bin/npm-cli.js --version
+
+# Restore our files
+echo "$our_gitreview" > .gitreview
+echo "$our_readme" > README-Wikimedia
+echo "$our_script" > .update.sh
+
+git add .
+git commit -m "Install $1
+
+From $url
+"
